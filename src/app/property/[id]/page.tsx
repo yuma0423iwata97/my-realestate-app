0db25d896f117300b9002a14afe8f93900014c18
yuma0423iwata97 <yuma0423iwataa@gmail.com>
2025-07-26 21:13:37 +0900

    fix: use correct PageProps on property detail page

--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -1,10 +1,7 @@
-/* app/property/[id]/page.tsx */
-
-interface PageProps {
+export default function PropertyDetail({
+  params,
+}: {
   params: { id: string };
-}
-
-export default async function PropertyDetail({ params }: PageProps) {
-  // ここで API フェッチしても OK
+}) {
   return <div className="p-4 text-xl">物件ID: {params.id}</div>;
 }
