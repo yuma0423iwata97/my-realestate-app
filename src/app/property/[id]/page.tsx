9027590d7b7834f0ae7c46fc8639f6c9bf1081c8
yuma0423iwata97 <yuma0423iwataa@gmail.com>
2025-07-26 21:57:19 +0900

    first commit

--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -1,7 +1,14 @@
 type Props = {
-  params: { id: string }
+  params: Promise<{ id: string }>;
+}
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
 }
 
-export default function PropertyDetail({ params }: Props) {
+export default async function PropertyDetail({ params }: Props) {
+	const { id: idString } = await params;
+	const id = parseInt(idString);
   return <div className="p-4 text-xl">物件詳細: {params.id}</div>;
 }
