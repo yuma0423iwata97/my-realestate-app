86b5e704b026cdf1cd12923d13a17939d899d473
CityClubHouseJP <Cityclubhouse.jp@gmail.com>
2026-01-27 10:02:14 +0000

    first commit

---
+++ src/app/property/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function PropertyDetail({ params }: Props) {
+	const { id: idString } = await params;
+	const id = parseInt(idString);
+  return <div className="p-4 text-xl">物件詳細: {id}</div>;
+}
